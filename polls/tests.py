5f4e3ac44c35669eedcfc9214b1e4af6fc015419
Alexhaoge <1810064@mail.nankai.edu.cn>
2021-03-31 20:51:24 +0800

    Update tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, client
+from django.contrib.auth import get_user_model
+from django.urls.base import reverse
+
+User = get_user_model()
 
 # Create your tests here.
+class UserTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        return super().setUpTestData()
+
+    def test_registration(self):
+        cli = client.Client()
+        resp = cli.post(reverse('register'), {
+            'username': 'a',
+            'password1': 'a',
+            'password2': 'a',
+            'email': 'a@a.com',
+            'last_name': 'a',
+            'first_name': 'a',
+            'person_id': 1,
+            'phone': '11111111111',
+        })
+        # print(resp.content)
+        self.assertContains(resp, '* person_id\n  *')

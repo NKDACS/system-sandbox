a46c19d99167547ba14b60c2ab2e82f6c48b39d2
hyZhanghy <75421568+hyZhanghy@users.noreply.github.com>
2021-04-03 08:15:38 +0800

    resume_change_1

--- polls/forms.py
+++ polls/forms.py
@@ -64,18 +64,23 @@ class ResumeForm(forms.ModelForm):
     class Meta:
         model = Resume
         fields = [ 
+            'aspiration', 'major_choices', 'tutor',
             'university', 'school', 'major', 'gpa',
             'rank', 'major_student_amount', 'cet6',
-            'other_prize_penalty', 'others',
+            'cet6_grades', 'other_prize_penalty', 'others',
         ]
         widgets = {
+            'aspiration': forms.Select(attrs={'class': 'form-control'}),
+            'major_choices': forms.Select(attrs={'class': 'form-control'}),
+            'tutor': forms.Select(attrs={'class': 'form-control'}),
             'university': forms.Select(attrs={'class': 'form-control'}),
             'school': forms.TextInput(attrs={'class': 'form-control'}),
             'major': forms.TextInput(attrs={'class': 'form-control'}),
             'gpa': forms.NumberInput(attrs={'class': 'form-control'}),
             'rank': forms.NumberInput(attrs={'class': 'form-control'}),
-            'major_student_amout': forms.NumberInput(attrs={'class': 'form-control'}),
+            'major_student_amount': forms.NumberInput(attrs={'class': 'form-control'}),
             'cet6': forms.CheckboxInput(attrs={'class': 'form-check-input', 'style': 'margin-left:1rem;'}),
+            'cet6_grades': forms.NumberInput(attrs={'class': 'form-control'}),
             'other_prize_penalty': forms.Textarea(attrs={'class': 'form-control', 'rows': '3', 'maxlength':'1000'}),
             'others': forms.Textarea(attrs={'class': 'form-control', 'rows': '3', 'maxlength': '1000'}),
         }
